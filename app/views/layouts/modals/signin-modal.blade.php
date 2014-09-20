441e2de00c80cdbf13c7691551ea13b74dc55668
nicklaw5 <nick_law@tpg.com.au>
2014-09-20 12:00:02 +1000

    changed login

--- app/views/layouts/modals/signin-modal.blade.php
+++ app/views/layouts/modals/signin-modal.blade.php
@@ -21,16 +21,16 @@
           {{ Form::open(['url' => 'foo/bar']) }}
 
             <div class="form-group">
-              {{ Form::label('username', 'Username') }} <span class="text-primary">*</span> <small class="text-danger form-error-message">This is an error.</small>
+              {{ Form::label('username', 'Username') }} <span class="text-primary">*</span> <!--<small class="text-danger form-error-message">This is an error.</small>-->
               {{ Form::text('username', '', ['class' => 'form-control br-none', 'placeholder' => 'Username', 'autocomplete' => 'off', 'required']) }}              
             </div>
             
             <div class="form-group">
-              {{ Form::label('password', 'Password') }} <span class="text-primary">*</span> <small class="text-danger form-error-message">This is an error.</small>
+              {{ Form::label('password', 'Password') }} <span class="text-primary">*</span> <!--<small class="text-danger form-error-message">This is an error.</small>-->
               {{ Form::password('password', ['class' => 'form-control br-none', 'placeholder' => 'Password', 'autocomplete' => 'off', 'required']) }}
             </div>
 
-            {{ Form::submit('Sign up', ['class' => 'btn btn-default btn-lg btn-block br-none']) }}
+            {{ Form::submit('Sign in', ['class' => 'btn btn-default btn-lg btn-block br-none']) }}
 
           {{ Form::close() }}
 
