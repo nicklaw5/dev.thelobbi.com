6e15eef6f2313c43c729a33abecff015c885bf82
nicklaw5 <nick_law@tpg.com.au>
2014-09-20 15:23:54 +1000

    changes

--- app/routes.php
+++ app/routes.php
@@ -6,8 +6,13 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
+
+
 Route::get('/facebook-signin', 'SessionsController@loginWithFacebook');
 Route::get('/google-signin', 'SessionsController@loginWithGoogle');
 
+Route::get('/oauth2callback', function() {
+	return;
+});
 
 Route::resource('sessions', 'SessionsController');
