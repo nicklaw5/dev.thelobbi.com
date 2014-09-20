014b91434773fa8cb83c1b247581081e0967487c
nicklaw5 <nick_law@tpg.com.au>
2014-09-20 15:51:33 +1000

    chnages

--- app/views/layouts/modals/signin-modal.blade.php
+++ app/views/layouts/modals/signin-modal.blade.php
@@ -11,7 +11,7 @@
           <p>Sign in with just one click using your social network account.</p>
           <li><a href="/facebook-signin" class="btn btn-default btn-lg btn-block btn-facebook-signin br-none"><i class="fa fa-facebook"></i> Sign in with Facebook</a></li>
           <li><a href="#" class="btn btn-default btn-lg btn-block btn-twitch-signin br-none"><i class="fa fa-twitch"></i> Sign in with Twitch</a></li>
-          <li><a href="#" class="btn btn-default btn-lg btn-block btn-twitter-signin br-none"><i class="fa fa-twitter"></i> Sign in with Twitter</a></li>
+          <li><a href="/twitter-signin" class="btn btn-default btn-lg btn-block btn-twitter-signin br-none"><i class="fa fa-twitter"></i> Sign in with Twitter</a></li>
           <li><a href="/google-signin" class="btn btn-default btn-lg btn-block btn-google-signin br-none"><i class="fa fa-google-plus"></i> Sign in with Google</a></li>
         </ul>
 
