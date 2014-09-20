2ac58c654eabe22496d9a7a52491e73492cd08ff
nicklaw5 <nick_law@tpg.com.au>
2014-09-20 15:52:24 +1000

    additional things

--- app/routes.php
+++ app/routes.php
@@ -11,6 +11,6 @@ Route::get('/', function()
 Route::get('/facebook-signin', 'SessionsController@loginWithFacebook');
 Route::get('/google-signin', 'SessionsController@loginWithGoogle');
 Route::get('/twitter-signin', 'SessionsController@loginWithTwitter');
-Route::get('/twitch-signin', 'SessionsController@loginWithTwitch')
+Route::get('/twitch-signin', 'SessionsController@loginWithTwitch');
 
 Route::resource('sessions', 'SessionsController');
