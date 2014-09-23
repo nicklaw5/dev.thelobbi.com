7e22aa14bb96366ce5c337194d7f1cc420f77c8d
nicklaw5 <nick_law@tpg.com.au>
2014-09-23 17:36:06 +1000

    chnages database details

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
+			'database'  => 'forg',
 			'username'  => 'forge',
 			'password'  => 'YSbYA7kc28Vlm1WDekWL',
 			'charset'   => 'utf8',
