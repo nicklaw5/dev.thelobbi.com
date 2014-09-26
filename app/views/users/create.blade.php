afd3fe387aa8051969cd717497b4ff56faf6958f
nicklaw5 <nick_law@tpg.com.au>
2014-09-26 15:06:21 +1000

    more changes

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -21,11 +21,6 @@
 		              {{ Form::text('username', Session::get('socialData')['username'], ['id' => 'username', 'class' => 'form-control br-none', 'placeholder' => 'Username', 'autocomplete' => 'off', 'required']) }}
 		            </div>
 
-		            <!--<div class="form-group">
-		              {{ Form::label('email', 'Email') }} <span class="text-primary">*</span> <small id="email-error" class="text-danger form-error-message pull-right"></small>
-		              {{ Form::email('email', Session::get('socialData')['email'], ['id' => 'email', 'class' => 'form-control br-none', 'placeholder' => 'Email', 'autocomplete' => 'off', 'required']) }}
-		            </div> -->
-
 		            <div class="form-group">
 		              {{ Form::label('password', 'Password') }} <span class="text-primary">*</span> <small id="password-error" class="text-danger form-error-message pull-right"></small>
 		              {{ Form::password('password', ['id' => 'password', 'class' => 'form-control br-none', 'placeholder' => 'Password', 'autocomplete' => 'off', 'required']) }}
