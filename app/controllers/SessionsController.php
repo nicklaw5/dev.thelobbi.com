06aec72b30699f95d0f5ae90537b18199238ff93
nicklaw5 <nick_law@tpg.com.au>
2014-09-28 10:43:16 +1000

    more changes

--- app/controllers/SessionsController.php
+++ app/controllers/SessionsController.php
@@ -17,29 +17,16 @@ class SessionsController extends BaseController {
 		// attempt to sign the user in, if successful
 		// return them to the page they came from
 		if(Auth::attempt(array(
-				'username' 	=> Input::get('username'),
-				'active'	=> (int) 1
-				)
-			)) {
-			//if unsuccessfull, return to login with error
-			Session::put('signinError', 'You haven\'t yet activated you account.');
-			return Redirect::to('/login')->withInput();
-		}
-
-
-		if(Auth::attempt(array(
 			'username' 	=> Input::get('username'),
 			'password' 	=> Input::get('password'),
+			'active'	=> (int) 1
 			)
 		))
-			return Redirect::back();
+			return Redirect::intended();
 
 		//if unsuccessfull, return to login with error
 		Session::put('signinError', 'Invalid username or password.');
 		return Redirect::to('/login')->withInput();
-
-
-		
 	}
 
 	public function destroy() {
